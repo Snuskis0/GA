cff5a4ecc7b108b5797dc818660cf8a927cdc352
Snuskis0 <simosk685@edu.linkoping.se>
2023-08-08 00:01:54 +0200

    graphics added (unused), editor started

---
+++ config.py
@@ -0,0 +1,7 @@
+# Screen
+screenX = 1024
+screenY = 512
+
+# Editor
+editorX = 0
+editorY = 0
--- main.py
+++ main.py
@@ -1,11 +1,36 @@
 # Imports
 import pygame
-import random
+from config import *
 import os
+
+os.system('cls')
+
+# Classes
+class Editor():
+    def __init__(self, origin):
+        self.origin = origin
+        self.background = pygame.image.load('Graphics/Backgrounds/bg_grasslands.png')
+        self.rect = self.background.get_rect(topleft = (editorX, editorY))
+    
+    def render(self):
+        screen.blit(self.background, self.rect)
+        
+    def setBgSize(self, size):
+        self.background = pygame.transform.scale(self.background, size)
+    
+    def update(self):
+        pass
+
+# Functions
+
 
 # Setup
 pygame.init()
+screen = pygame.display.set_mode((screenX, screenY))
 running = True
+
+editorOrigo = (editorX, editorY)
+editor = Editor(editorOrigo)
 
 # Main
 while running:
@@ -13,6 +38,13 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-                        
+    
+    screen.fill('White')
+    editor.render()
+    
+    
+    pygame.display.flip()
+# End of Main
+   
 pygame.quit()
 exit()
