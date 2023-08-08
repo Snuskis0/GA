ecb3f81cbf35d837a74b577a37ecedc9a5d6116d
Snuskis0 <simosk685@edu.linkoping.se>
2023-08-08 03:10:10 +0200

    added "show grid" on click

--- config.py
+++ config.py
@@ -5,3 +5,5 @@ screenY = 512
 # Editor
 editorX = 0
 editorY = 0
+blockW = 64 # 1024 / 64 = 16, blocks are 70x70 but changed to 60x60
+blockH = 64 # 1024 / 64 = 16
--- main.py
+++ main.py
@@ -20,8 +20,26 @@ class Editor():
     
     def update(self):
         pass
+    
+    def showGrid(self):
+        x = int(pygame.display.get_window_size()[0] / blockW)
+        y = int(pygame.display.get_window_size()[1] / blockH)
+        
+        for i in range(x):
+            for j in range(y):
+                pygame.draw.line(screen,'black',(0,j*blockW),(x*blockW,j*blockH))
+                pygame.draw.line(screen,'black',(i*blockW,0),(i*blockW,y*blockH))
+            
+    
+    
+
+class Obstacle(pygame.sprite.Sprite):
+    def __init__(self, ):
+        super().__init__()
+        
 
 # Functions
+
 
 
 # Setup
@@ -39,8 +57,15 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     
+        
+            
+    
     screen.fill('White')
     editor.render()
+    if pygame.mouse.get_pressed()[0]:
+        editor.showGrid()
+    
+    pygame.draw.circle(screen,'Red',pygame.mouse.get_pos(),5)
     
     
     pygame.display.flip()
