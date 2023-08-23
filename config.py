40d995d3750ee08ab9b7a02eee2c79462892e60e
Snuskis0 <simosk685@edu.linkoping.se>
2023-08-23 17:36:32 +0200

    Unlimited map (bugs)

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ screenX = 1024
 screenY = 512
 
 # Editor
-editorX = 0
-editorY = 0
+mapX = 0
+mapY = 0
 blockW = 70 # 1024 / 64 = 16, blocks are 70x70 but changed to 60x60
 blockH = 70 # 1024 / 64 = 16
 placeSpeedLimit = 100 #milliseconds
