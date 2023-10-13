7d826be34d57031a8ae3b5abb378a863a7f63714
Snuskis0 <simosk685@edu.linkoping.se>
2023-10-13 11:56:45 +0000

    Forgot to save (bugged), fixed now

--- Editor/Map/Block/FullBlock/Materials/grass.py
+++ Editor/Map/Block/FullBlock/Materials/grass.py
@@ -1,6 +1,4 @@
-import pygame
 from Editor.Map.Block.FullBlock.fullBlock import *
-from functions import howManyTrueIn
 
 class Grass(FullBlock):
     def __init__(self, pos):
--- Editor/Map/Block/block.py
+++ Editor/Map/Block/block.py
@@ -1,5 +1,6 @@
 import pygame
 from config import blockW, blockH
+from functions import howManyTrueIn
 
 class Block(pygame.sprite.Sprite):
     def __init__(self, pos, filename):
